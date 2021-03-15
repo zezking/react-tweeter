d506e5213de5d6faa5a99ba5b0e584ad2402e7c8
zezking <zhaoenze001@gmail.com>
2021-03-14 19:33:36 -0700

    refactor to following Francis' hooks tutorials

--- src/App.js
+++ src/App.js
@@ -6,11 +6,37 @@ import "./App.css";
 import { NewTweet } from "./NewTweet";
 import { Navigation } from "./Navigation";
 import { Header } from "./Header";
+import { TweetFeedCard } from "./TweetFeedCard";
+import { ConvertTime } from "./Helpers";
+let initialTWeetData = Tweets;
 
-const TweetsObj = Tweets;
-console.log(TweetsObj);
+console.log(initialTWeetData);
+
 const profile = "https://picsum.photos/200?random=1";
 export const App = () => {
+  const [tweetsData, setTweetData] = useState(initialTWeetData);
+
+  const addNewTweet = () => {
+    const newTWeet = {
+      name: "Sori Han",
+      handle: "@hansoriCS",
+      profile_image: "https://picsum.photos/200?random=20",
+      text: "I LOVE REFRIDRATORS!",
+      data: "10 days ago",
+    };
+  };
+  const tweets = tweetsData.map((tweetsData, key) => {
+    return (
+      <TweetFeedCard
+        key={key}
+        avatars={`https://picsum.photos/200?random=${key + 5}`}
+        name={tweetsData.user.name}
+        id={tweetsData.user.handle}
+        content={tweetsData.content.text}
+        created_at={ConvertTime(tweetsData.created_at)}
+      />
+    );
+  });
   return (
     <div className="App">
       <div className="top-nav">
@@ -21,9 +47,7 @@ export const App = () => {
         <main className="container">
           <NewTweet />
           <section className="new-tweet-feed">
-            <div className="new-tweet-post">
-              <TweetFeed tweets={Tweets} />
-            </div>
+            <div className="new-tweet-post">{tweets}</div>
           </section>
         </main>
       </div>

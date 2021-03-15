6a5a2f4acadd4452d7b87932a76eb45358596a35
zezking <zhaoenze001@gmail.com>
2021-03-15 13:59:49 -0700

    create a story for the tweetfeedcard componenent

---
+++ src/stories/Tweet.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { TweetFeedCard } from "../Componenets/TweetFeedCard";
+
+export default { title: "Singular Tweet" };
+
+export const emptyTweet = () => <TweetFeedCard />;
